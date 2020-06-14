0fa80b8f741d52415711c47449a327739cd20fdd
Lucas Santos <lucazwebmail@gmail.com>
2020-06-14 12:39:50 -0300

    feat: add gulp browser-sync function to live reload html  / sass files

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,34 @@
-const { series, src, dest } = require('gulp');
+const { series, src, dest, task, watch, parallel } = require('gulp');
 const concat = require('gulp-concat');
 const htmlReplace = require('gulp-html-replace');
 const cleanCss = require('gulp-clean-css');
 const del = require('del')
+
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+
+task('test', () => console.log('test'));
+
+task('css-browsersync', function() {
+  return src(["*.scss"])
+      .pipe(sass().on('error', function(err) {
+          console.error(err.message);
+          browserSync.notify(err.message, 3000); // Display error in the browser
+          this.emit('end'); // Prevent gulp from catching the error and exiting the watch process
+      }))
+      .pipe(dest('./'))
+      .pipe(browserSync.stream());
+});
+
+task('serve', series('css-browsersync', function(){
+  browserSync.init({
+    server: './'
+  });
+  watch('./*.scss', parallel('css-browsersync'));
+  watch('./*.html').on('change', browserSync.reload);
+}))
+
+
 
 function clean() {
   return del('build/');
