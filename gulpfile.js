ae200fcb8512158849bd569cc1c0ff045426052d
Lucas Santos <lucazwebmail@gmail.com>
2020-06-13 21:07:17 -0300

    feat: add build tasks to gulpfile
    - move files to build folder, minify css, remove html unecessary imports, change .gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,29 @@
-function defaultTask(cb) {
-  cb();
+const { series, src, dest } = require('gulp');
+const concat = require('gulp-concat');
+const htmlReplace = require('gulp-html-replace');
+const cleanCss = require('gulp-clean-css');
+const del = require('del')
+
+function clean() {
+  return del('build/');
 }
 
-exports.default = defaultTask;
+function styles() {
+  return src(['./node_modules/bootstrap/dist/css/bootstrap.min.css', 'style.css'])
+    .pipe(concat('style.css'))
+    .pipe(cleanCss())
+    .pipe(dest('build/'))
+}
+
+function htmlForBuild(){
+  return src('index.html')
+    .pipe(htmlReplace({ remove: ''}))
+    .pipe(dest('build/'))
+}
+
+function build() {
+  return src(['assets/**', 'js/**'], { base: '.'})
+    .pipe(dest('build/'));
+}
+
+exports.default = series(clean, styles, htmlForBuild, build);
